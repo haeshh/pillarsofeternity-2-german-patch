90c2da2d207feab262a670491b8e0f47cd9ac761
Aurelio <aureliosilverdawn@gmail.com>
2018-12-03 20:00:10 +0100

    Korrektur meiner eigenen Rechtschreibefehler

--- translation_helper/windows_example/check_text.bat
+++ translation_helper/windows_example/check_text.bat
@@ -1,13 +1,13 @@
-rem Ersetzte <drive_letter> mit dem Laufwerkbuchstaben, wo die Texte fuer von Pillars of Eternity: Deadfire liegen: z. B.: d
-rem Ersetzte <text_source> mit dem Pfad wo die Texte fuer von Pillars of Eternity: Deadfire liegen: z. B.: poe2_git
-rem Ersetzte <drive_letter_helper> mit dem Laufwerkbuchstaben, wo die Helper Dateien liegen: z. B.: d
-rem Ersetzte <helper_source> mit dem Pfad, wo die Helper Dateien liegen: z. B.: test_poe2
+rem Ersetze <drive_letter> mit dem Laufwerkbuchstaben, wo die Texte von "Pillars of Eternity: Deadfire" liegen: z. B.: d
+rem Ersetze <text_source> mit dem Pfad, wo die Texte von "Pillars of Eternity: Deadfire" liegen: z. B.: poe2_git
+rem Ersetze <drive_letter_helper> mit dem Laufwerkbuchstaben, wo die Helper Dateien liegen: z. B.: d
+rem Ersetze <helper_source> mit dem Pfad, wo die Helper Dateien liegen: z. B.: test_poe2
 rem
 rem Beispiel:
 rem java -jar saxon9he.jar -s:d:/PoE_2_git/exported/localized/de_patch/language.xml -xsl:d:/test_poe2/stringextract.xsl -threads:4
 rem java -jar languagetool-commandline.jar -r -l de-DE -c UTF-8 -d DE_CASE,UPPERCASE_SENTENCE_START d:\poe2_translation_bare > poe2_text_fehler.txt 2>&1
 rem
-rem In der Datei stringextract.xsl muss text_source noch angepasst werden. Muss der gleich Pfad wie hier in der Batch Datei sein. Also z. B.: PoE_2_git
+rem In der Datei stringextract.xsl muss text_source noch angepasst werden. Es muss der gleiche Pfad wie hier in dieser Batch Datei sein. Also z. B.: PoE_2_git
 rem Die erste Zeile erzeugt ein neues Verzeichnis poe2_translation_bare auf dem Laufwerk <drive_letter_helper>. Also hier im Beispiel dann auf d.
 
 
